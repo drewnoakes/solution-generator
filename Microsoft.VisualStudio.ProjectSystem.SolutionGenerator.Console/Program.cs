3464af6c2fed114020b9ddacebd8695b6cd7ec4b
Drew Noakes <git@drewnoakes.com>
2023-05-31 22:04:00 +1000

    Update to newer version of .NET

--- Microsoft.VisualStudio.ProjectSystem.SolutionGenerator.Console/Program.cs
+++ Microsoft.VisualStudio.ProjectSystem.SolutionGenerator.Console/Program.cs
@@ -1,21 +1,12 @@
-﻿namespace Microsoft.VisualStudio.ProjectSystem.SolutionGeneration.Console
-{
-    // TODO consider dotnet cli tool
+﻿using Microsoft.VisualStudio.ProjectSystem.SolutionGeneration;
 
-    internal static class Program
+new SolutionGenerator().Generate(
+    projectCount: 20,
+    new SdkProjectGenerator("netcoreapp2.1"),
+    new IProjectModifier[]
     {
-        public static void Main()
-        {
-            new SolutionGenerator().Generate(
-                projectCount: 20,
-                new SdkProjectGenerator("netcoreapp2.1"),
-                new IProjectModifier[]
-                {
-                    new CSharpClassFileCreator(itemCount: 10),
-                    new MaximalProjectToProjectReferences(),
-                    new PackageReferences("MetadataExtractor", "2.2.0")
-                },
-                @"d:\generated-solutions\maximal-p2p-20");
-        }
-    }
-}
+        new CSharpClassFileCreator(itemCount: 10),
+        new MaximalProjectToProjectReferences(),
+        new PackageReferences("MetadataExtractor", "2.2.0")
+    },
+    @"d:\generated-solutions\maximal-p2p-20");
